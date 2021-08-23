db92137be11e28f60137737647a7457102225c58
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-08-22 23:19:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+let breedsArray =  []
+const breedDropdown = document.querySelector("#breed-dropdown")
+const breedList = document.querySelector("#dog-breeds")
+
+breedDropdown.addEventListener("change", filterBreeds)
+
+function filterBreeds(event) {
+  const filteredBreeds = Object.keys(breedsArray).filter(breed => breed.startsWith(event.target.value))
+  breedList.innerHTML = ""
+  filteredBreeds.forEach(breed => {
+    breedLi = document.createElement("li")
+    breedLi.innerText = breed
+    breedLi.classList.add("breed-li")
+    breedLi.addEventListener("click", changeColor)
+    breedList.appendChild(breedLi)
+  })
+}
+
+fetch(imgUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.message.forEach(url => {
+      img = document.createElement("img")
+      img.src = url
+      document.querySelector("#dog-image-container").appendChild(img)
+    })
+  })
+
+function changeColor(event) {
+  event.target.style.color = "pink"
+}
+
+fetch(breedUrl)
+  .then(response => response.json())
+  .then(data => {
+    breedsArray = data.message
+    for (const breed in breedsArray) {
+      breedLi = document.createElement("li")
+      breedLi.innerText = breed
+      breedLi.classList.add("breed-li")
+      breedLi.addEventListener("click", changeColor)
+      breedList.appendChild(breedLi)
+    }
+  })
